5d630400c44a8aecfe8ad772a5f791f5ecd2b100
KeepCalm <hsj707932205@163.com>
2020-04-10 17:32:18 +0800

    单场比赛抓取

--- spy.py
+++ spy.py
@@ -2,7 +2,7 @@ from urllib.parse import urlencode
 import requests
 from lxml import etree
 
-url = "http://nba.win007.com/odds/AsianOdds_n.aspx?id=362243"
+url = "http://nba.win007.com/odds/AsianOdds_n.aspx?id=362541"
 ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10240"
 
 
@@ -12,20 +12,37 @@ def get_content():
         html = etree.HTML(content)
         return html
 
-def one_tr(html,num):
-        td_path = '/html/body/table[@id="odds"]/tr[{}]/td[not(@id="td_14" or @id="td_15" or @id="td_16")]'.format(num)
-        data = html.xpath(td_path)
-        for elem in data:
-            txt = elem.xpath('string(.)').strip()
-            print(txt)
-        # print(data)
 
-def all_tr():
+#庄家数据
+def bookie_data():
     html = get_content()
-    tr_path = '/html/body/table[@id="odds"]/tr[not(@style="display:none;")]'
+    tr_path = '/html/body/table[@id="odds"]/tr[not(@companyid)]'
+    td_path = './td[not(@id="td_14" or @id="td_15" or @id="td_16")]'
     data = html.xpath(tr_path)
     print(len(data))
-    for num in range(3,len(data)+1):
-        one_tr(html,num)
 
-all_tr()
+    for num in range(2,len(data)):
+        res = data[num].xpath(td_path)
+        for i in range(0,len(res)):
+            if(i != 1 and i != 8):
+                tx = res[i].xpath('string(.)').strip()
+                print(tx)
+
+
+#比赛数据
+def game_info():
+    html = get_content()
+    game_time = html.xpath('/html/body/div[@class="header"]/div[@class="analyhead"]/div[@class="vs"]/div[1]/text()')
+    home = html.xpath('/html/body/div[@class="header"]/div[@class="analyhead"]/div[@class="home"]/a/text()')
+    guest = html.xpath('/html/body/div[@class="header"]/div[@class="analyhead"]/div[@class="guest"]/a/text()')
+    score = html.xpath('/html/body/div[@class="header"]/div[@class="analyhead"]/div[@class="vs"]/div[@id="headVs"]/div[@class="end"]/div[@class="score"]/text()')
+    print(game_time[1])
+    print(home)
+    print(guest)
+    print(score)
+    # print(score[1])
+
+
+game_info()
+bookie_data()
+
