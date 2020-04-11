3ae0088d23cfe9f0ceeaad7b75d44ccb1ecadef0
KeepCalm <hsj707932205@163.com>
2020-04-11 17:31:05 +0800

    Add files via upload

--- spy.py
+++ spy.py
@@ -1,25 +1,26 @@
 from urllib.parse import urlencode
 import requests
 from lxml import etree
+import MySQLdb
 
-url = "http://nba.win007.com/odds/AsianOdds_n.aspx?id=362541"
+
 ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10240"
 
 
-def get_content():
+def get_content(url):
     with requests.request('GET',url,headers = {'User-agnet':ua}) as res:
         content = res.text
         html = etree.HTML(content)
         return html
 
 
-#庄家数据
-def bookie_data():
-    html = get_content()
+#盘口数据
+def bookie_data(url):
+    html = get_content(url)
     tr_path = '/html/body/table[@id="odds"]/tr[not(@companyid)]'
     td_path = './td[not(@id="td_14" or @id="td_15" or @id="td_16")]'
     data = html.xpath(tr_path)
-    print(len(data))
+    # print(len(data))
 
     for num in range(2,len(data)):
         res = data[num].xpath(td_path)
@@ -29,20 +30,35 @@ def bookie_data():
                 print(tx)
 
 
-#比赛数据
-def game_info():
-    html = get_content()
+#比赛信息
+def game_info(url):
+    html = get_content(url)
     game_time = html.xpath('/html/body/div[@class="header"]/div[@class="analyhead"]/div[@class="vs"]/div[1]/text()')
     home = html.xpath('/html/body/div[@class="header"]/div[@class="analyhead"]/div[@class="home"]/a/text()')
     guest = html.xpath('/html/body/div[@class="header"]/div[@class="analyhead"]/div[@class="guest"]/a/text()')
     score = html.xpath('/html/body/div[@class="header"]/div[@class="analyhead"]/div[@class="vs"]/div[@id="headVs"]/div[@class="end"]/div[@class="score"]/text()')
-    print(game_time[1])
-    print(home)
-    print(guest)
-    print(score)
+    print(game_time[1].strip())
+    print(home[0])
+    print(guest[0])
+    print(score[0])
+    print(score[1])
+
     # print(score[1])
 
 
-game_info()
-bookie_data()
+def save_data():
+    conn = MySQLdb.connect(
+        host = '127.0.0.1',
+        port = 3306,
+        user = 'root',
+        passwd = '3989313hsj',
+        db = 'nba_data',
+    )
+    print('存入数据库成功')
 
+
+# 10月份：362243-362307 11月份:
+for m_id in range(362243,362246):
+    url = "http://nba.win007.com/odds/AsianOdds_n.aspx?id={}".format(m_id)
+    game_info(url)
+    bookie_data(url)
